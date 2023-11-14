c8f6118cdd8e5493712b3cadc06117d7b04f15be
Gast-Account <simon.ortgiese@bpc.ag>
2023-11-14 14:56:03 +0100

    55

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-#from aleph_alpha_client import Client
+from aleph_alpha_client import Client
 import sys
 import json
 
@@ -6,8 +6,8 @@ import json
 
 if __name__ == "__main__":
 
-    #client = Client(
-    #    token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyMDMzNiwidG9rZW5faWQiOjM5MDZ9.yLAWL-PK-j9Hh6qxyXp9f_61HJInX3vbgZgzQSTjOyE")
+    client = Client(
+        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyMDMzNiwidG9rZW5faWQiOjM5MDZ9.yLAWL-PK-j9Hh6qxyXp9f_61HJInX3vbgZgzQSTjOyE")
 
     #sentence = sys.argv[1]
 
