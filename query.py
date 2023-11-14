c3f6511a2f3c63e8cd8ef26c630c2c4e0003350d
Gast-Account <simon.ortgiese@bpc.ag>
2023-11-14 14:43:45 +0100

    14:43

--- query.py
+++ query.py
@@ -1,13 +1,13 @@
-from aleph_alpha_client import Client
+#from aleph_alpha_client import Client
 import sys
 
 
 
 if __name__ == "__main__":
 
-    client = Client(
-        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyMDMzNiwidG9rZW5faWQiOjM5MDZ9.yLAWL-PK-j9Hh6qxyXp9f_61HJInX3vbgZgzQSTjOyE")
+    #client = Client(
+    #    token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyMDMzNiwidG9rZW5faWQiOjM5MDZ9.yLAWL-PK-j9Hh6qxyXp9f_61HJInX3vbgZgzQSTjOyE")
 
-    sentence = sys.argv[1]
+    #sentence = sys.argv[1]
 
-    print(sentence)
+    print("Hello worldd")
